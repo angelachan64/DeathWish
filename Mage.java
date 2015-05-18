85a34fb560809964c4bd5474f657591be1fed965
angelachan64 <angelachan64@gmail.com>
2015-05-18 08:39:52 -0400

    Mage

--- Mage.java
+++ Mage.java
@@ -5,7 +5,7 @@ public class Mage extends BaseChar{
     public Mage(){
 	health = 50; mana = 75; maxhealth = 50; maxmana = 75;
 	level = 1; exp = 0; maxexp = 20;
-	strength = 10; intelligence = 10; dexterity = 10; defense = 10;
+	strength = 8; intelligence = 15; dexterity = 10; defense = 7;
 	luck = 10; charisma = 10;
     }
 }
